329bb1f3e687aeacde23327516466ba0929b01e2
mari1647iv <ima1647@outlook.com>
2025-02-07 16:05:30 +0300

    quick fixes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import { topics } from "./constants";
 
 function App() {
   let [isLight, switchTheme] = useState(true)
-  let [isFormVisible, setIsFormVisible] = useState(false)
   let [data, setData] = useState(topics)
 
   return (
@@ -15,14 +14,8 @@ function App() {
         <h1>Header</h1>
       </header>
       <Display data={data} isLight={isLight}/>
-      <button className={isLight ? "light" : "dark"} onClick={() => { switchTheme(!isLight) }}>Switch Theme</button>
-      {
-          <>
-            <hr style={{width: "95%", border: "1px solid lightgrey"}}/>
-            <Form isLight={isLight} />
-            <button onClick={()=>{setIsFormVisible(!isFormVisible)}}>Скрыть форму</button>
-          </>
-      }
+      <hr style={{width: "95%", border: "1px solid lightgrey"}}/>
+      <Form isLight={isLight} />
     </div >
   );
 }
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -2,38 +2,18 @@ import { useReducer } from "react";
 
 function Form({isLight}) {
 
-  function formDataReducer(state, updatedData) {
-    if (!!updatedData 
-      && ((!updatedData.hasOwnProperty("name")) || (updatedData.name.length>0)) 
-      && ((!updatedData.hasOwnProperty("description")) || (updatedData.description?.length>0)) 
-      && ((!updatedData.hasOwnProperty("link")) || (updatedData.link?.length>0))
-    ) {
-      let newstate = Object.assign(state, updatedData)
-      return newstate
-    }
-    return state
-  }
-
-  const [ formdata, setformdata ] = useReducer(formDataReducer, {
-    name: "Name",
-    description: "Description",
-    link: "Link"
-  })
-
-  function changeEventHandler(event) {
-    let tempdata = {}
-    tempdata[event.target.name] = event.target.value
-    setformdata(tempdata)
-  }
-
-  console.log(formdata)
+  const formdata = [
+    ["name", ""],
+    ["description", ""],
+    ["link", ""]
+  ]
 
   return (
     <div className={(isLight ? "light" : "dark")}>
       <form onSubmit={(e)=>{e.preventDefault()}}>
         {
           Array.from(formdata).map((field)=>(
-            <input type="text" placeholder={"Enter topic "+field[0]} name={field[0]} value={field[1]} onChange={changeEventHandler}/>
+            <input type="text" placeholder={"Enter topic "+field[0]} name={field[0]} value={field[1]}/>
           ))
         }
         <button className={isLight ? "light" : "dark"} type='submit' onClick={()=>{}} >Add Technology</button>
