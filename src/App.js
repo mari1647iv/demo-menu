7014ce61bcc950dd046b1632973d0c320e57ac10
mari1647iv <ima1647@outlook.com>
2025-02-07 15:11:42 +0300

    add form

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 import Display from "./components/Display/Display";
 import Form from "./components/Form/Form";
-import apiService from './apiService';
+import { topics } from "./constants";
 
 function App() {
   let [isLight, switchTheme] = useState(true)
-  let [data, setData] = useState([])
-
-  useEffect(()=>{
-    apiService.get().then((resp)=>{
-      console.log(resp)
-      setData(resp)
-    }).catch((error)=>{
-      console.log(error)
-    })
-  }, [])
-
+  let [isFormVisible, setIsFormVisible] = useState(false)
+  let [data, setData] = useState(topics)
 
   return (
     <div className={"App " + (isLight ? "light" : "dark")}>
@@ -25,10 +16,43 @@ function App() {
       </header>
       <Display data={data} isLight={isLight}/>
       <button className={isLight ? "light" : "dark"} onClick={() => { switchTheme(!isLight) }}>Switch Theme</button>
-      <Form isLight={isLight}/>
+      {
+          <>
+            <hr style={{width: "95%", border: "1px solid lightgrey"}}/>
+            <Form isLight={isLight} />
+            <button onClick={()=>{setIsFormVisible(!isFormVisible)}}>Скрыть форму</button>
+          </>
+      }
+      <CustomButton >
+        Кликов: 
+      </CustomButton>
     </div >
   );
 }
+
+function useCustomState(init)  {
+  if (!init || (typeof(inti)!=="number") || ((4 < init) && (init < 16))) { init = 0}
+  else {init=init}
+  let [state, setState] = useState(init)
+
+  return [state, setState]
+}
+
+function CustomButton({children, initialState}) {
+  let [state, setState] = useCustomState(67)
+
+   function onClick() { setState(state+1) }
+
+   return (
+      <button className="red-button fancy-button" type="button" onClick={onClick}>
+         {children} {state}
+      </button>
+   )
+}
+
+
+
+
 
 
 export default App;
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,14 +1,41 @@
-import { useEffect, useState } from 'react';
-
+import { useReducer } from "react";
 
 function Form({isLight}) {
+
+  function formDataReducer(state, updatedData) {
+    if (!!updatedData 
+      && ((!updatedData.hasOwnProperty("name")) || (updatedData.name.length>0)) 
+      && ((!updatedData.hasOwnProperty("description")) || (updatedData.description?.length>0)) 
+      && ((!updatedData.hasOwnProperty("link")) || (updatedData.link?.length>0))
+    ) {
+      let newstate = Object.assign(state, updatedData)
+      return newstate
+    }
+    return state
+  }
+
+  const [ formdata, setformdata ] = useReducer(formDataReducer, {
+    name: "Name",
+    description: "Description",
+    link: "Link"
+  })
+
+  function changeEventHandler(event) {
+    let tempdata = {}
+    tempdata[event.target.name] = event.target.value
+    setformdata(tempdata)
+  }
+
+  console.log(formdata)
 
   return (
     <div className={(isLight ? "light" : "dark")}>
       <form onSubmit={(e)=>{e.preventDefault()}}>
-        <input type="text" placeholder="Enter Course Name"/>
-        <input type="text" placeholder="Enter Course Description"/>
-        <input type="text" placeholder="Provide Link to Documentation"/>
+        {
+          Array.from(formdata).map((field)=>(
+            <input type="text" placeholder={"Enter topic "+field[0]} name={field[0]} value={field[1]} onChange={changeEventHandler}/>
+          ))
+        }
         <button className={isLight ? "light" : "dark"} type='submit' onClick={()=>{}} >Add Technology</button>
       </form>
     </div >
